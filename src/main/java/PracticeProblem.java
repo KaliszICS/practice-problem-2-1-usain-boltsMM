8fc28e47a45df0a0b7a71e2d5e86f9a9b8017be7
usain-boltsMM <343421046@tdsb.ca>
2026-03-10 20:44:55 +0000

    funcionado

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,30 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+    public static void main(String[] args) {
 
-	}
+    }
 
-	//Create your functions here
+    public static int sum(int num1, int num2) {
+        int result;
+        result = num1 + num2;
+        return result;
+    }
 
+    public static int difference(int num1, int num2) {
+        int result;
+        result = num1 - num2;
+        return result;
+    }
+
+    public static double product(double num1, double num2) {
+        double result;
+        result = num1 * num2;
+        return result;
+    }
+
+    public static String removeFirst(String word1) {
+        String result;
+        result = word1.substring(1);
+        return result;
+    }
 }
